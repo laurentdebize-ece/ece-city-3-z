9e0789a2d56df05cf8b6f2f8156b0bed19cf7e2e
ilaydakocer <ilayda.kocer@edu.ece.fr>
2022-10-31 15:57:18 +0100

    Structure de base incomplete pour le deroulement du jeu

--- graphe.h
+++ graphe.h
@@ -2,8 +2,21 @@
 #ifndef ECE_CITY_3_Z_GRAPHE_H
 #define ECE_CITY_3_Z_GRAPHE_H
 //Le .h ou on mettra toutes les fonctions en lien avec le graphe et le déroulement du jeu
+enum construction{terrainVague, cabane, maison, immeuble, gratteCiel};
+//                   0             1       2      3          4
 
 
+typedef struct {
+    int niveau; // 0 = sur terre   -1 = canalisation       -2 = réseau d'electricité
+    int carence;  // 0 si le batiment est bien alimenté en eau/electricité      1 si il y a une carrence d'eau ou d'electricté
+}Niveau;
+
+
+typedef struct {
+    int etat; //etat des construction -> on utilisera l'enum "construction" pour que ce soit plus facile
+    Niveau mapAffiche;   // par exemple si on a case[0][5].mapAfficher.niveau == 1  ->  on affichera la canalisation
+
+}Case;  //chaque case correspond a un sommet de notre graphe
 
 
 
