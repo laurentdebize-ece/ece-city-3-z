716974cf865a8fe578aa21143b04782b9135bacc
ilaydakocer <ilayda.kocer@edu.ece.fr>
2022-10-31 15:41:08 +0100

    J'aii crer le graphe.h et .c donc pas besoin que vous le faite.
    Theophile tu devrais creer un .h et .c juste pour l'affiches graphique je pense

---
+++ graphe.c
@@ -0,0 +1,5 @@
+//
+// Created by Ilayda Kocer on 31/10/2022.
+//
+
+#include "graphe.h"
---
+++ graphe.h
@@ -0,0 +1,12 @@
+
+#ifndef ECE_CITY_3_Z_GRAPHE_H
+#define ECE_CITY_3_Z_GRAPHE_H
+//Le .h ou on mettra toutes les fonctions en lien avec le graphe et le déroulement du jeu
+
+
+
+
+
+
+
+#endif //ECE_CITY_3_Z_GRAPHE_H
