16224ea2d96b7c1aedfa6de11a91c0aa0e7c70d5
ilaydakocer <ilayda.kocer@edu.ece.fr>
2022-11-01 14:55:17 +0100

    fichier texte inclut dans le dossier du projet (est-ce qu'il s'ouvre pour tout le monde ? )

--- graphe.c
+++ graphe.c
@@ -5,6 +5,7 @@
 
 Graphe* CreerGraphe(int ordre)
 {
+
     Graphe* Newgraphe=malloc(sizeof(Graphe));
     Newgraphe->tabSommet = malloc(ordre*sizeof(Sommet));
 
@@ -18,19 +19,23 @@ Graphe* CreerGraphe(int ordre)
             Newgraphe->tabSommet[i][j].valeur = i*j;
             Newgraphe->tabSommet[i][j].etat = 0;   //il n'y a aucune construction au debut
         }
+
     }
     return Newgraphe;
 }
 
 Graphe * lire_graphe(char * nomFichier)
 {
+
     Graphe* graphe;
     FILE * ifs = fopen(nomFichier,"r+");   //r+ pour lecture ET ecriture dans un fichier texte déjà crée
     int taille, ordre;
     if (!ifs){
+
         printf("Erreur de lecture fichier\n");
         exit(-1);
     }
+
 
     fscanf(ifs,"%d",&ordre);
     graphe=CreerGraphe(ordre); // pn creer un graphe de 1444 sommets (38*38)
